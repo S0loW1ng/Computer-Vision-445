89f82769d86fefe28cc91ec949a8f71ed5c7e991
S0loW1ng <cadet.calderon@gmail.com>
2019-03-07 11:50:04 -0800

    GEtting the histogram working

--- MainScript.m
+++ MainScript.m
@@ -4,17 +4,22 @@ I = imread('29.jpg');
 Ipross = 1-im2bw(I);
 imshow(Ipross);
 % clean the holes
-SE = strel('disk',5);
-SE2= strel('disk',3);
-SE3 = strel('square',10);
+SE = strel('square',5);
+SE2= strel('square',2);
+SE3 = strel('square',3);
 %cleans the immage to make it more square 
 IE = imclose(Ipross,SE);
 IE = imopen(IE,SE2);
-IE = imopen(IE,SE3);
+IE = imdilate(IE,SE3);
 %gets the edge blobs removed.
 Im = imclearborder(IE,8);
-figure, imshow(Im)
-
 %we can ge the the area of the blobs and put in a histogram.
+[L,N] = bwlabel(IE);
 
+figure, imshow(label2rgb(L));
+f = regionprops(L);
 
+% histogram
+areas = cat(2,f(:).Area);
+
+histogram(areas);
