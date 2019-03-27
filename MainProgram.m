9966a294c4e0268b7a26c19cf211c1d1716cf701
Acaparro <acaparroso97@gmail.com>
2019-03-27 16:12:20 -0700

    Added my code into the main script

--- MainProgram.m
+++ MainProgram.m
@@ -2,14 +2,15 @@ I = imread('24.jpg');
 
 %We get the image clean Eunice and Yue
 % we detec if there are holes or not Adrian
-
+b = hasHoles(I);
 % if holes
-
-%count holes
-backup(I);
-%Count threads
-verical = VerticalThreadCount(I)
-hori = HorizontalThreadCount(I)
-%if not
-%count interThreads
-inter = InterFunction(I)
+if( b == 1)
+    %count holes
+    backup(I);
+    %Count threads
+    verical = VerticalThreadCount(I);
+    hori = HorizontalThreadCount(I);
+    %if not
+    %count interThreads
+    inter = InterFunction(I)
+end
